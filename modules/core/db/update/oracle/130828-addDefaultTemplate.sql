9d62873ffb8bc95dccc56a575e5d44e70531a020
Eugeny Degtyarjov <degtyarjov@haulmont.com>
2013-08-28 08:30:48 +0000

    update scripts #PL-2449

---
+++ modules/core/db/update/oracle/130828-addDefaultTemplate.sql
@@ -0,0 +1,6 @@
+--$Id$--
+alter table REPORT_REPORT add column DEFAULT_TEMPLATE_ID varchar2(32);
+
+alter table REPORT_REPORT add constraint FK_REPORT_TO_DEF_TEMPLATE foreign key (DEFAULT_TEMPLATE_ID) references REPORT_TEMPLATE(ID)^
+
+
---
+++ modules/core/db/update/postgres/13/130828-addDefaultTemplate.sql
@@ -0,0 +1,6 @@
+--$Id$--
+alter table REPORT_REPORT add column DEFAULT_TEMPLATE_ID uuid;
+
+alter table REPORT_REPORT add constraint FK_REPORT_REPORT_TO_DEF_TEMPLATE foreign key (DEFAULT_TEMPLATE_ID)
+references REPORT_TEMPLATE (ID);
+
