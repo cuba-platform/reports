3857b1ebe1bf67133ff93ac54287c23932a03a7c
Sergey Saiyan <saiyan@haulmont.com>
2013-09-11 15:52:32 +0000

    fix MSSQL update scripts  #PL-2587 Fixed

--- modules/core/db/update/mssql/13/130830-newStructure.sql
+++ modules/core/db/update/mssql/13/130830-newStructure.sql
@@ -1,5 +1,5 @@
 --$Id$--
-alter table REPORT_REPORT add column XML varchar(max)^
-alter table REPORT_TEMPLATE add column CONTENT image^
-alter table REPORT_TEMPLATE add column NAME varchar(500)^
+alter table REPORT_REPORT add XML varchar(max)^
+alter table REPORT_TEMPLATE add CONTENT image^
+alter table REPORT_TEMPLATE add NAME varchar(500)^
 
