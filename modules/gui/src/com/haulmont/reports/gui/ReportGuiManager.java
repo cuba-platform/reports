3e766ac728fe419b2e1998b8d7ef3eeb8c6259a4
Konstantin Krivopustov <krivopustov@haulmont.com>
2013-09-16 10:23:29 +0000

    Update copyright text

--- modules/gui/src/com/haulmont/reports/gui/ReportGuiManager.java
+++ modules/gui/src/com/haulmont/reports/gui/ReportGuiManager.java
@@ -1,12 +1,6 @@
 /*
- * Copyright (c) 2012 Haulmont Technology Ltd. All Rights Reserved.
- * Haulmont Technology proprietary and confidential.
- * Use is subject to license terms.
-
- * Author: degtyarjov
- * Created: 11.09.13 12:01
- *
- * $Id$
+ * Copyright (c) 2008-2013 Haulmont. All rights reserved.
+ * Use is subject to license terms, see http://www.cuba-platform.com/license for details.
  */
 package com.haulmont.reports.gui;
 
