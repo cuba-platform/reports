b5e0788cb1bb1c01b8a3afb1d0b30e353e53d697
Aleksey Kozyaikin <kozyaikin@haulmont.com>
2014-05-22 14:12:48 +0000

    ReportTemplatePlaceholder. Fix problems for fields  contains entities value. #PL-3828

--- modules/core/src/com/haulmont/reports/wizard/template/ReportTemplatePlaceholder.java
+++ modules/core/src/com/haulmont/reports/wizard/template/ReportTemplatePlaceholder.java
@@ -10,7 +10,6 @@ import com.haulmont.cuba.core.global.AppBeans;
 import com.haulmont.cuba.core.global.Messages;
 import com.haulmont.reports.entity.wizard.RegionProperty;
 import com.haulmont.reports.entity.wizard.ReportRegion;
-import org.apache.commons.lang.BooleanUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -23,10 +22,13 @@ import java.util.Date;
  * @version $Id$
  */
 public class ReportTemplatePlaceholder {
+    protected static final String HAS_CONTENT = "?has_content";
+    protected static final String AND = " && ";
     protected static final String TABLE_MASK = "${%s}";
     protected static final String COMMON_MASK = "${%s.%s}";
-    protected static final String HTML_COMMON_MASK = "${%s.fields('%s')!?string!}"; //like ${Task[0].fields('id')!?string!}
-    protected static final String HTML_DATE_MASK = "<#if %1$s.fields('%2$s')?has_content>${%1$s.fields('%2$s')?string(\"%3$s\")}</#if>";// /like <#if Task[0].fields('updateTs')?has_content>${Task[0].fields('updateTs')?string("dd.MM.yyyy hh:mm")}</#if>
+    protected static final String HTML_VALUE_MASK = "%s.fields('%s')%s";
+    protected static final String HTML_COMMON_MASK = "%4$s${(%1$s.fields('%2$s')%3$s)!?string!}%5$s"; //like ${Task[0].fields('id')!?string!}
+    protected static final String HTML_DATE_MASK = "<#if %5$s %1$s.fields('%2$s')%4$s?has_content>${%1$s.fields('%2$s')%4$s?string(\"%3$s\")}</#if>";// /like <#if Task[0].fields('updateTs')?has_content>${Task[0].fields('updateTs')?string("dd.MM.yyyy hh:mm")}</#if>
     protected Log log = LogFactory.getLog(getClass());
 
     /**
@@ -54,9 +56,7 @@ public class ReportTemplatePlaceholder {
         String fieldName = regionProperty.getEntityTreeNode().getHierarchicalNameExceptRoot();
         if (reportRegion.isTabulatedRegion()) {
             bandName = "row";
-            if (BooleanUtils.isNotFalse(reportRegion.getReportData().getIsTabulatedReport())) {
-               fieldName = StringUtils.removeStart(regionProperty.getEntityTreeNode().getHierarchicalNameExceptRoot(), reportRegion.getRegionPropertiesRootNode().getName() + ".");
-            }
+            fieldName = StringUtils.removeStart(regionProperty.getEntityTreeNode().getHierarchicalNameExceptRoot(), reportRegion.getRegionPropertiesRootNode().getName() + ".");
         } else {
             bandName = reportRegion.getNameForBand() + "[0]";
         }
@@ -82,9 +82,47 @@ public class ReportTemplatePlaceholder {
             } else {
                 dateMask = messages.getMessage(messages.getMainMessagePack(), "dateTimeFormat");
             }
-            return String.format(HTML_DATE_MASK, bandName, fieldName, dateMask);
+            String[] partsFieldName = fieldName.split("\\.");
+            if (partsFieldName.length > 1) {
+                fieldName = partsFieldName[0];
+            }
+            return String.format(HTML_DATE_MASK, bandName, fieldName, dateMask, generatePathForEntityField(partsFieldName), generateConditions(partsFieldName, bandName, fieldName, false));
         } else {
-            return String.format(HTML_COMMON_MASK, bandName, fieldName);
+            String[] partsFieldName = fieldName.split("\\.");
+            if (partsFieldName.length > 1) {
+                fieldName = partsFieldName[0];
+            }
+            String condition = generateConditions(partsFieldName, bandName, fieldName, true);
+            return String.format(HTML_COMMON_MASK, bandName, fieldName, generatePathForEntityField(partsFieldName), condition, condition.length() > 0 ? "</#if>" : "");
         }
+    }
+
+    protected String generatePathForEntityField(String[] partsFieldName) {
+        StringBuilder pathForEntityField = new StringBuilder();
+        if (partsFieldName.length > 1) {
+            for (int i = 1; i < partsFieldName.length; i++) {
+                pathForEntityField.append(".").append(partsFieldName[i]);
+            }
+        }
+        return pathForEntityField.toString();
+    }
+
+    protected String generateConditions(String[] partsFieldName, String bandName, String fieldName, boolean addIf) {
+        StringBuilder pathForEntityField = new StringBuilder();
+        StringBuilder condition = new StringBuilder();
+        if (partsFieldName.length > 1) {
+            condition.append(String.format(HTML_VALUE_MASK, bandName, fieldName, pathForEntityField.toString())).append(HAS_CONTENT).append(AND);
+            for (int i = 1; i < partsFieldName.length; i++) {
+                pathForEntityField.append(".").append(partsFieldName[i]);
+                if (i < partsFieldName.length - 1)
+                    condition.append(" ").append(String.format(HTML_VALUE_MASK, bandName, fieldName, pathForEntityField.toString())).append(HAS_CONTENT).append(AND);
+            }
+            if (condition.length() > 4 && addIf) {
+                condition.delete(condition.length() - 4, condition.length());
+                condition.insert(0, "<#if ");
+                condition.append(">");
+            }
+        }
+        return condition.toString();
     }
 }
