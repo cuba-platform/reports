43c76bafe98cb1dc18757a3bd742a3b8cc847a5e
Eugeny Degtyarjov <degtyarjov@haulmont.com>
2015-06-23 14:28:39 +0000

    PL-5544 Add ability to show report band data on Cuba charts #PL-5544

--- modules/gui/src/com/haulmont/reports/gui/report/run/ShowChartController.java
+++ modules/gui/src/com/haulmont/reports/gui/report/run/ShowChartController.java
@@ -8,9 +8,11 @@ package com.haulmont.reports.gui.report.run;
 import com.haulmont.cuba.gui.components.*;
 import com.haulmont.cuba.gui.data.ValueListener;
 import com.haulmont.cuba.gui.theme.ThemeConstants;
+import com.haulmont.cuba.gui.xml.layout.ComponentsFactory;
 import com.haulmont.reports.entity.Report;
 import com.haulmont.reports.gui.ReportGuiManager;
 import com.haulmont.yarg.reporting.ReportOutputDocument;
+import org.apache.commons.collections.CollectionUtils;
 
 import javax.annotation.Nullable;
 import javax.inject.Inject;
@@ -46,6 +48,9 @@ public class ShowChartController extends AbstractWindow {
     @Inject
     private Label reportLookupLabel;
 
+    @Inject
+    private ComponentsFactory componentsFactory;
+
     protected InputParametersController inputParametersController;
 
     protected Report report;
@@ -66,6 +71,8 @@ public class ShowChartController extends AbstractWindow {
             reportLookup.setVisible(false);
             reportLookupLabel.setVisible(false);
             initFrames(chartJson);
+        } else {
+            showDiagramStubText();
         }
 
         reportLookup.addListener(new ValueListener() {
@@ -109,6 +116,18 @@ public class ShowChartController extends AbstractWindow {
         if (chartJson != null) {
             openFrame(chartBox, JSON_CHART_SCREEN_ID,
                     Collections.<String, Object>singletonMap("Chart", chartJson));
+        }
+
+        showDiagramStubText();
+    }
+
+    private void showDiagramStubText() {
+        if (CollectionUtils.isEmpty(chartBox.getComponents())) {
+            Label label = componentsFactory.createComponent(Label.NAME);
+            label.setValue(getMessage("showChart.caption"));
+            label.setAlignment(Alignment.MIDDLE_CENTER);
+            label.setStyleName("h1");
+            chartBox.add(label);
         }
     }
 }
