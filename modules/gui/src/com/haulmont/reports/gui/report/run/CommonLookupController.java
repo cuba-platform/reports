0ab3aa90b8aedb145f05f7e139c53f28893d3f69
Eugeny Degtyarjov <degtyarjov@haulmont.com>
2015-12-29 13:18:48 +0000

    PL-6498 Entities without instance name are not displayed while running the report View changed to local Filter added to simplify filtering and control number of rows #PL-6498

--- modules/gui/src/com/haulmont/reports/gui/report/run/CommonLookupController.java
+++ modules/gui/src/com/haulmont/reports/gui/report/run/CommonLookupController.java
@@ -8,9 +8,7 @@ import com.haulmont.chile.core.model.MetaClass;
 import com.haulmont.cuba.core.global.Messages;
 import com.haulmont.cuba.core.global.View;
 import com.haulmont.cuba.gui.AppConfig;
-import com.haulmont.cuba.gui.components.AbstractLookup;
-import com.haulmont.cuba.gui.components.BoxLayout;
-import com.haulmont.cuba.gui.components.Table;
+import com.haulmont.cuba.gui.components.*;
 import com.haulmont.cuba.gui.data.CollectionDatasource;
 import com.haulmont.cuba.gui.data.DsBuilder;
 import com.haulmont.cuba.gui.xml.layout.ComponentsFactory;
@@ -44,8 +42,9 @@ public class CommonLookupController extends AbstractLookup {
         CollectionDatasource cds = new DsBuilder(getDsContext())
                 .setId("mainDs")
                 .setMetaClass(metaClass)
-                .setViewName(View.MINIMAL)
+                .setViewName(View.LOCAL)
                 .buildCollectionDatasource();
+        cds.setQuery("select e from " + metaClass.getName() + " e");
 
         final Table table = cFactory.createComponent(Table.class);
         table.setId("lookupTable");
@@ -55,9 +54,28 @@ public class CommonLookupController extends AbstractLookup {
         table.setWidth("100%");
         table.setHeight("100%");
         table.requestFocus();
-        mainPane.add(table);
 
-        table.refresh();
+        RowsCount rowsCount = cFactory.createComponent(RowsCount.class);
+        rowsCount.setDatasource(cds);
+        rowsCount.setOwner(table);
+        table.setRowsCount(rowsCount);
+
+        Filter filter = cFactory.createComponent(Filter.class);
+        filter.setFrame(this);
+        filter.setId("filter");
+        filter.setApplyTo(table);
+        filter.setDatasource(cds);
+        filter.setMaxResults(100);
+        filter.setUseMaxResults(true);
+        filter.setManualApplyRequired(true);
+        filter.setEditable(true);
+        ((FilterImplementation) filter).loadFiltersAndApplyDefault();
+
+        mainPane.add(filter);
+        mainPane.add(table);
+        mainPane.expand(table);
+
+        filter.apply(true);
 
         this.setLookupComponent(table);
     }
