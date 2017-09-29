b601de19ba52c11da220f49ceddec5a043de915b
Andrey Subbotin <subbotin@haulmont.com>
2017-09-29 21:38:12 +0400

    PL-9183 Add soft delete to REPORT_REPORT (fix oracle scripts)

--- modules/core/db/update/oracle/17/170825-addSoftDeleteColumns.sql
+++ modules/core/db/update/oracle/17/170825-addSoftDeleteColumns.sql
@@ -1,11 +1,11 @@
-alter table REPORT_REPORT add column DELETE_TS timestamp^
+alter table REPORT_REPORT add DELETE_TS timestamp^
 alter table REPORT_REPORT add column DELETED_BY varchar2(50)^
 
-alter table REPORT_GROUP add column DELETE_TS timestamp^
-alter table REPORT_GROUP add column DELETED_BY varchar2(50)^
+alter table REPORT_GROUP add DELETE_TS timestamp^
+alter table REPORT_GROUP add DELETED_BY varchar2(50)^
 
-alter table REPORT_TEMPLATE add column DELETE_TS timestamp^
-alter table REPORT_TEMPLATE add column DELETED_BY varchar2(50)^
+alter table REPORT_TEMPLATE add DELETE_TS timestamp^
+alter table REPORT_TEMPLATE add DELETED_BY varchar2(50)^
 
 drop index IDX_REPORT_REPORT_UNIQ_NAME^
 create unique index IDX_REPORT_REPORT_UNIQ_NAME on REPORT_REPORT (NAME, DELETE_TS)^
