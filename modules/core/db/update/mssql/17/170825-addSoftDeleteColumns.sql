e5b39c1fddb310c9e2fb17135770ee71288d55b1
Yuriy Artamonov <artamonov@haulmont.com>
2017-10-31 19:38:11 +0400

    PL-9944 Unable to recreate IDX_REPORT_REPORT_UNIQ_NAME index if it does not exist during update to 6.7

--- modules/core/db/update/mssql/17/170825-addSoftDeleteColumns.sql
+++ modules/core/db/update/mssql/17/170825-addSoftDeleteColumns.sql
@@ -7,8 +7,14 @@ alter table REPORT_GROUP add DELETED_BY varchar(50)^
 alter table REPORT_TEMPLATE add DELETE_TS datetime^
 alter table REPORT_TEMPLATE add DELETED_BY varchar(50)^
 
-drop index IDX_REPORT_REPORT_UNIQ_NAME on REPORT_REPORT^
-create unique index IDX_REPORT_REPORT_UNIQ_NAME on REPORT_REPORT (NAME, DELETE_TS)^
+if exists (select * from sys.indexes where name='IDX_REPORT_REPORT_UNIQ_NAME' AND object_id = OBJECT_ID('REPORT_REPORT'))
+begin
+  drop index IDX_REPORT_REPORT_UNIQ_NAME on REPORT_REPORT;
+  create unique index IDX_REPORT_REPORT_UNIQ_NAME on REPORT_REPORT (NAME, DELETE_TS);
+end^
 
-drop index IDX_REPORT_GROUP_UNIQ_TITLE on REPORT_GROUP^
-create unique index IDX_REPORT_GROUP_UNIQ_TITLE on REPORT_GROUP (TITLE, DELETE_TS)^
+if exists (select * from sys.indexes where name='IDX_REPORT_GROUP_UNIQ_TITLE' AND object_id = OBJECT_ID('REPORT_GROUP'))
+begin
+  drop index IDX_REPORT_GROUP_UNIQ_TITLE on REPORT_GROUP;
+  create unique index IDX_REPORT_GROUP_UNIQ_TITLE on REPORT_GROUP (TITLE, DELETE_TS);
+end^
--- modules/core/db/update/postgres/17/170825-addSoftDeleteColumns.sql
+++ modules/core/db/update/postgres/17/170825-addSoftDeleteColumns.sql
@@ -7,8 +7,36 @@ alter table REPORT_GROUP add column DELETED_BY varchar(50)^
 alter table REPORT_TEMPLATE add column DELETE_TS timestamp^
 alter table REPORT_TEMPLATE add column DELETED_BY varchar(50)^
 
-drop index IDX_REPORT_REPORT_UNIQ_NAME^
-create unique index IDX_REPORT_REPORT_UNIQ_NAME on REPORT_REPORT (NAME) where DELETE_TS is null^
+create function updateIndexReportUniqueName() returns void as $$
+begin
+  if exists (
+    select 1
+    from   pg_class c
+    join   pg_namespace n ON n.oid = c.relnamespace
+    where  c.relname = 'idx_report_report_uniq_name'
+    ) then
+      drop index IDX_REPORT_REPORT_UNIQ_NAME;
+      create unique index IDX_REPORT_REPORT_UNIQ_NAME on REPORT_REPORT (NAME) where DELETE_TS is null;
+  end if;
+end;
+$$ language plpgsql^
 
-drop index IDX_REPORT_GROUP_UNIQ_TITLE^
-create unique index IDX_REPORT_GROUP_UNIQ_TITLE on REPORT_GROUP (TITLE) where DELETE_TS is null^
+select updateIndexReportUniqueName()^
+drop function updateIndexReportUniqueName()^
+
+create function updateIndexReportGroupUniqueTitle() returns void as $$
+begin
+  if exists (
+    select 1
+    from   pg_class c
+    join   pg_namespace n ON n.oid = c.relnamespace
+    where  c.relname = 'idx_report_group_uniq_title'
+    ) then
+      drop index IDX_REPORT_GROUP_UNIQ_TITLE;
+      create unique index IDX_REPORT_GROUP_UNIQ_TITLE on REPORT_GROUP (TITLE) where DELETE_TS is null;
+  end if;
+end;
+$$ language plpgsql^
+
+select updateIndexReportGroupUniqueTitle()^
+drop function updateIndexReportGroupUniqueTitle()^
