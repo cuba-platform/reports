dd1e8666f3b69209c22d27614797fca4b22c1b74
Andrey Subbotin <subbotin@haulmont.com>
2018-07-04 15:29:49 +0400

    TablePrintFormAction.printAll method should not use paging parameters #88

--- modules/gui/src/com/haulmont/reports/gui/actions/TablePrintFormAction.java
+++ modules/gui/src/com/haulmont/reports/gui/actions/TablePrintFormAction.java
@@ -137,9 +137,6 @@ public class TablePrintFormAction extends AbstractPrintFormAction {
         parameterPrototype.setQueryString(query.getQueryString());
         parameterPrototype.setQueryParams(query.getParameters());
         parameterPrototype.setViewName(loadContext.getView().getName());
-        parameterPrototype.setFirstResult(query.getFirstResult());
-        parameterPrototype.setMaxResults(query.getMaxResults());
-
         openRunReportScreen(ComponentsHelper.getWindow(table), parameterPrototype, metaClass);
     }
 }
