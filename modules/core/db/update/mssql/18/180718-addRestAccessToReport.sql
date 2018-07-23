59c12117a47f6bbb46857a54f3b5c91ae722b8f4
Andrey Subbotin <subbotin@haulmont.com>
2018-07-23 10:03:24 +0400

    REST API for Reporting add-on #14
    Fix update scripts

--- modules/core/db/update/mssql/18/180718-addRestAccessToReport.sql
+++ modules/core/db/update/mssql/18/180718-addRestAccessToReport.sql
@@ -1,2 +1,2 @@
 alter table REPORT_REPORT add REST_ACCESS tinyint default 0^
-update REPORT_REPORT set REST_ACCESS = 0 where  is null^REST_ACCESS
+update REPORT_REPORT set REST_ACCESS = 0 where REST_ACCESS is null^
