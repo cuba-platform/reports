06ad66029de087f012d96b31fbf8abd0d5c07044
Andrey Subbotin <subbotin@haulmont.com>
2018-09-11 17:22:22 +0400

    REST API for Reporting add-on #14

--- modules/core/db/update/oracle/18/180718-addRestAccessToReport.sql
+++ modules/core/db/update/oracle/18/180718-addRestAccessToReport.sql
@@ -1,2 +1,2 @@
-alter table REPORT_REPORT add REST_ACCESS boolean default false^
+alter table REPORT_REPORT add REST_ACCESS char(1)^
 update REPORT_REPORT set REST_ACCESS = 0 where REST_ACCESS is null^
--- modules/core/db/update/oracle/18/180730-addSystemToReport.sql
+++ modules/core/db/update/oracle/18/180730-addSystemToReport.sql
@@ -1,2 +1,2 @@
-alter table REPORT_REPORT add IS_SYSTEM boolean default false^
+alter table REPORT_REPORT add IS_SYSTEM char(1)^
 update REPORT_REPORT set IS_SYSTEM = 0 where IS_SYSTEM is null^
