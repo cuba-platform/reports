15bc785237b85a68ecb5ff5276287e4ff0843d94
Sergey Fedorov Evgenyevich <s.fedorov@haulmont.com>
2018-11-07 13:03:09 +0400

    Implement ListPrintFormAction #116

---
+++ modules/gui/src/com/haulmont/reports/gui/actions/ListPrintFormAction.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright (c) 2008-2018 Haulmont. All rights reserved.
+ * Use is subject to license terms, see http://www.cuba-platform.com/commercial-software-license for details.
+ */
+
+package com.haulmont.reports.gui.actions;
+
+import com.google.common.base.Preconditions;
+import com.haulmont.chile.core.model.MetaClass;
+import com.haulmont.cuba.core.global.AppBeans;
+import com.haulmont.cuba.core.global.LoadContext;
+import com.haulmont.cuba.core.global.Messages;
+import com.haulmont.cuba.gui.ComponentsHelper;
+import com.haulmont.cuba.gui.WindowManager;
+import com.haulmont.cuba.gui.components.*;
+import com.haulmont.cuba.gui.data.CollectionDatasource;
+import com.haulmont.cuba.gui.data.Datasource;
+import com.haulmont.cuba.gui.screen.ScreenContext;
+import com.haulmont.reports.app.ParameterPrototype;
+import com.haulmont.reports.gui.ReportGuiManager;
+import org.apache.commons.collections4.CollectionUtils;
+
+import java.util.Set;
+
+public class ListPrintFormAction extends AbstractPrintFormAction {
+
+    protected Window window;
+    protected ListComponent listComponent;
+
+    public ListPrintFormAction(ListComponent listComponent) {
+        this("listComponentReport", listComponent);
+    }
+
+    public ListPrintFormAction(String id, ListComponent listComponent) {
+        super(id);
+
+        this.listComponent = listComponent;
+        Messages messages = AppBeans.get(Messages.NAME);
+        this.caption = messages.getMessage(ListPrintFormAction.class, "actions.Report");
+        this.icon = "icons/reports-print.png";
+    }
+
+    @Override
+    public void actionPerform(Component component) {
+        DialogAction cancelAction = new DialogAction(DialogAction.Type.CANCEL);
+
+        ScreenContext screenContext = ComponentsHelper.getScreenContext(listComponent);
+        Preconditions.checkState(window != null, "Component is not attached to window");
+
+        if (beforeActionPerformedHandler != null) {
+            if (!beforeActionPerformedHandler.beforeActionPerformed())
+                return;
+        }
+
+        WindowManager wm = (WindowManager) screenContext.getScreens();
+        Set selected = listComponent.getSelected();
+        if (CollectionUtils.isNotEmpty(selected)) {
+            if (selected.size() > 1) {
+                Action printSelectedAction = new AbstractAction("actions.printSelected", Status.PRIMARY) {
+                    @Override
+                    public void actionPerform(Component component) {
+                        printSelected(selected);
+                    }
+                };
+                printSelectedAction.setIcon("icons/reports-print-row.png");
+
+                Action printAllAction = new AbstractAction("actions.printAll") {
+                    @Override
+                    public void actionPerform(Component component) {
+                        printAll();
+                    }
+                };
+                printAllAction.setIcon("icons/reports-print-all.png");
+
+                Messages messages = AppBeans.get(Messages.NAME);
+                wm.showOptionDialog(messages.getMessage(ReportGuiManager.class, "notifications.confirmPrintSelectedheader"),
+                        messages.getMessage(ReportGuiManager.class, "notifications.confirmPrintSelected"),
+                        Frame.MessageType.CONFIRMATION,
+                        new Action[]{printAllAction, printSelectedAction, cancelAction});
+            } else {
+                printSelected(selected);
+            }
+        } else {
+            Messages messages = AppBeans.get(Messages.NAME);
+            CollectionDatasource ds = listComponent.getDatasource();
+
+            if ((ds.getState() == Datasource.State.VALID) && (ds.size() > 0)) {
+                Action yesAction = new DialogAction(DialogAction.Type.OK) {
+                    @Override
+                    public void actionPerform(Component component) {
+                        printAll();
+                    }
+                };
+
+                cancelAction.setPrimary(true);
+
+                wm.showOptionDialog(messages.getMessage(ListPrintFormAction.class, "notifications.confirmPrintAllheader"),
+                        messages.getMessage(ListPrintFormAction.class, "notifications.confirmPrintAll"),
+                        Frame.MessageType.CONFIRMATION, new Action[]{yesAction, cancelAction});
+            } else {
+                wm.showNotification(messages.getMessage(ReportGuiManager.class, "notifications.noSelectedEntity"),
+                        Frame.NotificationType.HUMANIZED);
+            }
+        }
+    }
+
+    protected void printSelected(Set selected) {
+        CollectionDatasource datasource = listComponent.getDatasource();
+        MetaClass metaClass = datasource.getMetaClass();
+        openRunReportScreen(ComponentsHelper.getWindow(listComponent), selected, metaClass);
+    }
+
+    protected void printAll() {
+        CollectionDatasource datasource = listComponent.getDatasource();
+
+        MetaClass metaClass = datasource.getMetaClass();
+
+        LoadContext loadContext = datasource.getCompiledLoadContext();
+
+        ParameterPrototype parameterPrototype = new ParameterPrototype(metaClass.getName());
+        parameterPrototype.setMetaClassName(metaClass.getName());
+        LoadContext.Query query = loadContext.getQuery();
+        parameterPrototype.setQueryString(query.getQueryString());
+        parameterPrototype.setQueryParams(query.getParameters());
+        parameterPrototype.setViewName(loadContext.getView().getName());
+        openRunReportScreen(ComponentsHelper.getWindow(listComponent), parameterPrototype, metaClass);
+    }
+}
+
--- modules/gui/src/com/haulmont/reports/gui/actions/TablePrintFormAction.java
+++ modules/gui/src/com/haulmont/reports/gui/actions/TablePrintFormAction.java
@@ -5,28 +5,10 @@
 
 package com.haulmont.reports.gui.actions;
 
-import com.google.common.base.Preconditions;
-import com.haulmont.chile.core.model.MetaClass;
-import com.haulmont.cuba.core.global.AppBeans;
-import com.haulmont.cuba.core.global.LoadContext;
-import com.haulmont.cuba.core.global.Messages;
-import com.haulmont.cuba.gui.ComponentsHelper;
-import com.haulmont.cuba.gui.WindowManager;
-import com.haulmont.cuba.gui.components.*;
-import com.haulmont.cuba.gui.components.DialogAction.Type;
-import com.haulmont.cuba.gui.data.CollectionDatasource;
-import com.haulmont.cuba.gui.data.Datasource;
-import com.haulmont.cuba.gui.screen.ScreenContext;
-import com.haulmont.reports.app.ParameterPrototype;
-import com.haulmont.reports.gui.ReportGuiManager;
-import org.apache.commons.collections4.CollectionUtils;
+import com.haulmont.cuba.gui.components.Table;
+import com.haulmont.cuba.gui.components.Window;
 
-import java.util.Set;
-
-public class TablePrintFormAction extends AbstractPrintFormAction {
-
-    protected Window window;
-    protected final Table table;
+public class TablePrintFormAction extends ListPrintFormAction {
 
     /**
      * @deprecated Use {@link TablePrintFormAction#TablePrintFormAction(Table)} instead.
@@ -41,14 +23,9 @@ public class TablePrintFormAction extends AbstractPrintFormAction {
      */
     @Deprecated
     public TablePrintFormAction(String id, Window window, Table table) {
-        super(id);
+        super(id, table);
 
         this.window = window;
-        this.table = table;
-
-        Messages messages = AppBeans.get(Messages.NAME);
-        this.caption = messages.getMessage(TablePrintFormAction.class, "actions.Report");
-        this.icon = "icons/reports-print.png";
     }
 
     public TablePrintFormAction(Table table) {
@@ -56,101 +33,6 @@ public class TablePrintFormAction extends AbstractPrintFormAction {
     }
 
     public TablePrintFormAction(String id, Table table) {
-        super(id);
-
-        this.table = table;
-
-        Messages messages = AppBeans.get(Messages.NAME);
-        this.caption = messages.getMessage(TablePrintFormAction.class, "actions.Report");
-        this.icon = "icons/reports-print.png";
-    }
-
-    @Override
-    public void actionPerform(Component component) {
-        DialogAction cancelAction = new DialogAction(Type.CANCEL);
-
-        ScreenContext screenContext = ComponentsHelper.getScreenContext(table);
-        Preconditions.checkState(window != null, "Table is not attached to window");
-
-        if (beforeActionPerformedHandler != null) {
-            if (!beforeActionPerformedHandler.beforeActionPerformed())
-                return;
-        }
-
-        WindowManager wm = (WindowManager) screenContext.getScreens();
-
-        Set selected = table.getSelected();
-        if (CollectionUtils.isNotEmpty(selected)) {
-            if (selected.size() > 1) {
-                Action printSelectedAction = new AbstractAction("actions.printSelected", Status.PRIMARY) {
-                    @Override
-                    public void actionPerform(Component component) {
-                        printSelected(selected);
-                    }
-                };
-                printSelectedAction.setIcon("icons/reports-print-row.png");
-
-                Action printAllAction = new AbstractAction("actions.printAll") {
-                    @Override
-                    public void actionPerform(Component component) {
-                        printAll();
-                    }
-                };
-                printAllAction.setIcon("icons/reports-print-all.png");
-
-                Messages messages = AppBeans.get(Messages.NAME);
-
-                wm.showOptionDialog(messages.getMessage(ReportGuiManager.class, "notifications.confirmPrintSelectedheader"),
-                        messages.getMessage(ReportGuiManager.class, "notifications.confirmPrintSelected"),
-                        Frame.MessageType.CONFIRMATION,
-                        new Action[]{printAllAction, printSelectedAction, cancelAction});
-            } else {
-                printSelected(selected);
-            }
-        } else {
-            CollectionDatasource ds = table.getDatasource();
-
-            Messages messages = AppBeans.get(Messages.NAME);
-
-            if ((ds.getState() == Datasource.State.VALID) && (ds.size() > 0)) {
-                Action yesAction = new DialogAction(Type.OK) {
-                    @Override
-                    public void actionPerform(Component component) {
-                        printAll();
-                    }
-                };
-
-                cancelAction.setPrimary(true);
-
-                wm.showOptionDialog(messages.getMessage(TablePrintFormAction.class, "notifications.confirmPrintAllheader"),
-                        messages.getMessage(TablePrintFormAction.class, "notifications.confirmPrintAll"),
-                        Frame.MessageType.CONFIRMATION, new Action[]{yesAction, cancelAction});
-            } else {
-                wm.showNotification(messages.getMessage(ReportGuiManager.class, "notifications.noSelectedEntity"),
-                        Frame.NotificationType.HUMANIZED);
-            }
-        }
-    }
-
-    protected void printSelected(Set selected) {
-        CollectionDatasource datasource = table.getDatasource();
-        MetaClass metaClass = datasource.getMetaClass();
-        openRunReportScreen(ComponentsHelper.getWindow(table), selected, metaClass);
-    }
-
-    protected void printAll() {
-        CollectionDatasource datasource = table.getDatasource();
-
-        MetaClass metaClass = datasource.getMetaClass();
-
-        LoadContext loadContext = datasource.getCompiledLoadContext();
-
-        ParameterPrototype parameterPrototype = new ParameterPrototype(metaClass.getName());
-        parameterPrototype.setMetaClassName(metaClass.getName());
-        LoadContext.Query query = loadContext.getQuery();
-        parameterPrototype.setQueryString(query.getQueryString());
-        parameterPrototype.setQueryParams(query.getParameters());
-        parameterPrototype.setViewName(loadContext.getView().getName());
-        openRunReportScreen(ComponentsHelper.getWindow(table), parameterPrototype, metaClass);
+        super(id, table);
     }
 }
