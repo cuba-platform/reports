4ace08e48d5eff8bd4769fd07d3e526193293c48
Sergey Fedorov Evgenyevich <s.fedorov@haulmont.com>
2018-12-13 13:08:47 +0400

    Add meaningful error message for importing a report with the same ID #98

--- modules/core/src/com/haulmont/reports/ReportingBean.java
+++ modules/core/src/com/haulmont/reports/ReportingBean.java
@@ -122,7 +122,7 @@ public class ReportingBean implements ReportingApi {
             List<ReportTemplate> existingTemplates = null;
             try {
                 existingReport = em.find(Report.class, report.getId(), "report.withTemplates");
-
+                report = em.merge(report);
                 if (existingReport != null) {
                     report.setVersion(existingReport.getVersion());
                     if (existingReport.getTemplates() != null) {
@@ -132,14 +132,14 @@ public class ReportingBean implements ReportingApi {
                         existingReport.setDeleteTs(null);
                         existingReport.setDeletedBy(null);
                     }
+                    report.setDefaultTemplate(null);
+                    report.setTemplates(null);
                 } else {
                     report.setVersion(0);
                 }
 
                 storeIndexFields(report);
                 dynamicAttributesManagerAPI.storeDynamicAttributes(report);
-
-                report = em.merge(report);
 
                 if (loadedTemplates != null) {
                     if (existingTemplates != null) {
