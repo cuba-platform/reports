b1e74d49fd6b4585530c623c19f7af04ed494beb
subbotin <subbotin@haulmont.com>
2020-01-20 18:44:48 +0400

    Fix Mysql, Oracle script #178

--- modules/core/db/update/mysql/19/190730-add-execution-output-document.sql
+++ modules/core/db/update/mysql/19/190730-add-execution-output-document.sql
@@ -1,5 +1,5 @@
 
-alter table REPORT_EXECUTION add column OUTPUT_DOCUMENT_ID varchar(32)^
+alter table REPORT_EXECUTION add OUTPUT_DOCUMENT_ID varchar(32)^
 
 alter table REPORT_EXECUTION add constraint FK_REPORT_EXECUTION_TO_DOCUMENT
 foreign key (OUTPUT_DOCUMENT_ID) references SYS_FILE(ID)^
--- modules/core/db/update/oracle/19/190730-add-execution-output-document.sql
+++ modules/core/db/update/oracle/19/190730-add-execution-output-document.sql
@@ -1,5 +1,5 @@
 
-alter table REPORT_EXECUTION add column OUTPUT_DOCUMENT_ID varchar2(32)^
+alter table REPORT_EXECUTION add OUTPUT_DOCUMENT_ID varchar2(32)^
 
 alter table REPORT_EXECUTION add constraint FK_REPORT_EXECUTION_TO_DOCUMEN
 foreign key (OUTPUT_DOCUMENT_ID) references SYS_FILE(ID)^
